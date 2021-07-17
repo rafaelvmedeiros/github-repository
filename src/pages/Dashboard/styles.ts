30958dea7f97d44f8150c7888797ca6a05275556
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-07-17 13:07:37 -0300

    feat: change input line when takes error

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -9,7 +13,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -19,9 +23,15 @@ export const Form = styled.form`
     height: 70px;
     padding: 0 24px;
     border: 0;
-    border-radius: 5px 0 0 5px;
-    border: 1px solid #ccc;
     color: #3a3a3a;
+    border-radius: 5px 0 0 5px;
+    border: 2px solid #fff;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #ff1919;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -95,4 +105,12 @@ export const Repositories = styled.div`
       transform: translateX(10px);
     }
   }
+`;
+
+export const Error = styled.div`
+  display: block;
+  margin-top: 20px;
+  font-size: 18px;
+  font-weight: bold;
+  color: #ff1919;
 `;
